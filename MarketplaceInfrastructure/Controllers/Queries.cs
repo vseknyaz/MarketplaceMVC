99144a3607553a1dbef216f4bd7b3e531003ae0a
root <root@DESKTOP-SQPUCEH>
2025-05-15 21:42:23 +0300

    working with great design usgin accordion, vanishing tabs

--- MarketplaceInfrastructure/Controllers/Queries.cs
+++ MarketplaceInfrastructure/Controllers/Queries.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using MarketplaceInfrastructure; // Припускаємо, що це ваш контекст БД
+using MarketplaceInfrastructure;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace MarketplaceInfrastructure.Controllers
 {
@@ -15,150 +14,155 @@ namespace MarketplaceInfrastructure.Controllers
             _context = context;
         }
 
-        // GET: Відображення сторінки з формами
+        // GET: /Queries
         public IActionResult Index()
         {
             return View();
         }
 
-        // POST: Запит 1 - Міста магазинів із товарами дорожче X
+        // 1. Адреси магазинів із товарами дорожче X
         [HttpPost]
-        public IActionResult Query1(decimal price)
+        public IActionResult Query1(decimal priceThreshold)
         {
-            var result = _context.Shops
-                .Join(_context.Products, s => s.ShopId, p => p.ShopId, (s, p) => new { s, p })
-                .Where(x => x.p.Price > price)
-                .Select(x => x.s.Address)
+            var addresses = _context.Shops
+                .Where(s => s.Products.Any(p => p.Price > priceThreshold))
+                .Select(s => s.Address)
                 .Distinct()
                 .ToList();
-            ViewBag.Query1Result = result;
-            ViewBag.Query1Price = price;
+
+            ViewBag.PriceThreshold = priceThreshold;
+            ViewBag.Query1Result = addresses;
             return View("Index");
         }
 
-        // POST: Запит 2 - Клієнти, які зробили замовлення після дати X
+        // 2. Клієнти, які зробили замовлення після дати X
         [HttpPost]
-        public IActionResult Query2(DateTime date)
+        public IActionResult Query2(DateTime orderDateAfter)
         {
-            var result = _context.Clients
-                .Join(_context.Orders, c => c.ClientId, o => o.ClientId, (c, o) => new { c, o })
-                .Where(x => x.o.OrderDate > date)
-                .Select(x => x.c.FullName)
+            var clients = _context.Clients
+                .Where(c => c.Orders.Any(o => o.OrderDate > orderDateAfter))
+                .Select(c => c.FullName)
                 .Distinct()
                 .ToList();
-            ViewBag.Query2Result = result;
-            ViewBag.Query2Date = date;
+
+            ViewBag.OrderDateAfter = orderDateAfter;
+            ViewBag.Query2Result = clients;
             return View("Index");
         }
 
-        // POST: Запит 3 - Товари з категорії X
+        // 3. Товари з категорії X
         [HttpPost]
-        public IActionResult Query3(string categoryName)
+        public IActionResult Query3(string categoryNameProducts)
         {
-            var result = _context.Products
-                .Join(_context.Categories,
-                      p => p.CategoryId,
-                      c => c.CategoryName, // Використовуємо CategoryName, а не CategoryId
-                      (p, c) => new { p, c })
-                .Where(x => x.c.CategoryName == categoryName)
-                .Select(x => x.p.ProductName)
+            var products = _context.Products
+                .Where(p => p.CategoryId == categoryNameProducts)
+                .Select(p => p.ProductName)
                 .ToList();
-            ViewBag.Query3Result = result;
-            ViewBag.Query3CategoryName = categoryName;
+
+            ViewBag.CategoryNameProducts = categoryNameProducts;
+            ViewBag.Query3Result = products;
             return View("Index");
         }
 
-        // POST: Запит 4 - Замовлення з кількістю товарів більше X
+        // 4. Замовлення з кількістю товарів більше X
         [HttpPost]
-        public IActionResult Query4(int quantity)
+        public IActionResult Query4(int quantityThreshold)
         {
-            var result = _context.Orders
-                .Join(_context.OrderProducts, o => o.OrderId, op => op.OrderId, (o, op) => new { o, op })
-                .GroupBy(x => x.o.OrderId)
-                .Select(g => new { OrderId = g.Key, TotalQuantity = g.Sum(x => x.op.Quantity) })
-                .Where(x => x.TotalQuantity > quantity)
+            var orders = _context.OrderProducts
+                .GroupBy(op => op.OrderId)
+                .Select(g => new
+                {
+                    OrderId = g.Key,
+                    TotalQuantity = g.Sum(op => op.Quantity)
+                })
+                .Where(x => x.TotalQuantity > quantityThreshold)
                 .ToList();
-            ViewBag.Query4Result = result;
-            ViewBag.Query4Quantity = quantity;
+
+            ViewBag.QuantityThreshold = quantityThreshold;
+            ViewBag.Query4Result = orders;
             return View("Index");
         }
 
-        // POST: Запит 5 - Магазини з товарами на складі менше X
+        // 5. Магазини з товарами на складі менше X
         [HttpPost]
-        public IActionResult Query5(int stock)
+        public IActionResult Query5(int stockThreshold)
         {
-            var result = _context.Shops
-                .Join(_context.Products, s => s.ShopId, p => p.ShopId, (s, p) => new { s, p })
-                .Where(x => x.p.Stock < stock)
-                .Select(x => x.s.ShopName)
+            var shops = _context.Shops
+                .Where(s => s.Products.Sum(p => p.Stock) < stockThreshold)
+                .Select(s => s.ShopName)
                 .Distinct()
                 .ToList();
-            ViewBag.Query5Result = result;
-            ViewBag.Query5Stock = stock;
+
+            ViewBag.StockThreshold = stockThreshold;
+            ViewBag.Query5Result = shops;
             return View("Index");
         }
 
-        // POST: Запит 6 - Клієнти, які замовили всі товари з категорії X
+        // 6. Клієнти, які замовили всі товари з категорії X
         [HttpPost]
-        public IActionResult Query6(string categoryName)
+        public IActionResult Query6(string categoryNameClients)
         {
-            var result = _context.Clients
-                .Where(c => !_context.Products
-                    .Where(p => p.CategoryId == categoryName)
-                    .Any(p => !_context.Orders
-                        .Join(_context.OrderProducts, o => o.OrderId, op => op.OrderId, (o, op) => new { o, op })
-                        .Any(x => x.o.ClientId == c.ClientId && x.op.ProductId == p.ProductId)))
+            var productIds = _context.Products
+                .Where(p => p.CategoryId == categoryNameClients)
+                .Select(p => p.ProductId)
+                .ToList();
+
+            var clients = _context.Clients
+                .Where(c => productIds.All(pid =>
+                    c.Orders.SelectMany(o => o.OrderProducts)
+                     .Any(op => op.ProductId == pid)))
                 .Select(c => c.FullName)
                 .ToList();
-            ViewBag.Query6Result = result;
-            ViewBag.Query6CategoryName = categoryName;
+
+            ViewBag.CategoryNameClients = categoryNameClients;
+            ViewBag.Query6Result = clients;
             return View("Index");
         }
 
-        // POST: Запит 7 - Магазини, що продають ті самі товари, що й магазин X
+        // 7. Магазини, що продають ті самі товари, що й магазин X
         [HttpPost]
         public IActionResult Query7(int shopId)
         {
-            var result = _context.Shops
-                .Where(s2 => s2.ShopId != shopId
-                    && !_context.Products
-                        .Where(p => p.ShopId == shopId)
-                        .Any(p => !_context.Products
-                            .Where(p2 => p2.ShopId == s2.ShopId)
-                            .Select(p2 => p2.ProductId)
-                            .Contains(p.ProductId))
-                    && !_context.Products
-                        .Where(p2 => p2.ShopId == s2.ShopId)
-                        .Any(p2 => !_context.Products
-                            .Where(p => p.ShopId == shopId)
-                            .Select(p => p.ProductId)
-                            .Contains(p2.ProductId)))
-                .Select(s2 => s2.ShopName)
+            var baseIds = _context.Products
+                .Where(p => p.ShopId == shopId)
+                .Select(p => p.ProductId)
                 .ToList();
-            ViewBag.Query7Result = result;
-            ViewBag.Query7ShopId = shopId;
+
+            var shops = _context.Shops
+                .Where(s => s.ShopId != shopId
+                            && baseIds.All(id => s.Products.Select(p => p.ProductId).Contains(id))
+                            && s.Products.Select(p => p.ProductId).All(id => baseIds.Contains(id)))
+                .Select(s => s.ShopName)
+                .ToList();
+
+            ViewBag.ShopId = shopId;
+            ViewBag.Query7Result = shops;
             return View("Index");
         }
 
-        // POST: Запит 8 - Пари клієнтів, які замовили однакові товари
+        // 8. Пари клієнтів, які замовили однакові товари
         [HttpPost]
         public IActionResult Query8()
         {
-            var result = (from c1 in _context.Clients
-                          from c2 in _context.Clients
-                          where c1.ClientId < c2.ClientId
-                          && _context.Orders
-                              .Join(_context.OrderProducts, o => o.OrderId, op => op.OrderId, (o, op) => new { o, op })
-                              .Where(x => x.o.ClientId == c1.ClientId)
-                              .Select(x => x.op.ProductId)
-                              .Intersect(_context.Orders
-                                  .Join(_context.OrderProducts, o => o.OrderId, op => op.OrderId, (o, op) => new { o, op })
-                                  .Where(x => x.o.ClientId == c2.ClientId)
-                                  .Select(x => x.op.ProductId))
-                              .Any()
-                          select new { Client1 = c1.FullName, Client2 = c2.FullName })
-             .ToList();
-            ViewBag.Query8Result = result;
+            var pairs = (from c1 in _context.Clients
+                         from c2 in _context.Clients
+                         where c1.ClientId < c2.ClientId
+                            && _context.OrderProducts
+                                .Where(op => op.Order.ClientId == c1.ClientId)
+                                .Select(op => op.ProductId)
+                                .Intersect(
+                                    _context.OrderProducts
+                                        .Where(op => op.Order.ClientId == c2.ClientId)
+                                        .Select(op => op.ProductId)
+                                ).Any()
+                         select new
+                         {
+                             Client1 = c1.FullName,
+                             Client2 = c2.FullName
+                         })
+                        .ToList();
+
+            ViewBag.Query8Result = pairs;
             return View("Index");
         }
     }
---
+++ MarketplaceInfrastructure/Models/QueriesViewModel.cs
@@ -0,0 +1,52 @@
+﻿// File: ViewModels/QueriesViewModel.cs
+using System;
+using System.Collections.Generic;
+
+namespace YourNamespace.ViewModels
+{
+    public class OrderQuantityDto
+    {
+        public int OrderId { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+
+    public class ClientPairDto
+    {
+        public string Client1 { get; set; }
+        public string Client2 { get; set; }
+    }
+
+    public class QueriesViewModel
+    {
+        // Query 1
+        public decimal? PriceThreshold { get; set; }
+        public List<string> Query1Result { get; set; }
+
+        // Query 2
+        public DateTime? OrderDateAfter { get; set; }
+        public List<string> Query2Result { get; set; }
+
+        // Query 3
+        public string CategoryNameProducts { get; set; }
+        public List<string> Query3Result { get; set; }
+
+        // Query 4
+        public int? QuantityThreshold { get; set; }
+        public List<OrderQuantityDto> Query4Result { get; set; }
+
+        // Query 5
+        public int? StockThreshold { get; set; }
+        public List<string> Query5Result { get; set; }
+
+        // Query 6
+        public string CategoryNameClients { get; set; }
+        public List<string> Query6Result { get; set; }
+
+        // Query 7
+        public int? ShopId { get; set; }
+        public List<string> Query7Result { get; set; }
+
+        // Query 8
+        public List<ClientPairDto> Query8Result { get; set; }
+    }
+}
