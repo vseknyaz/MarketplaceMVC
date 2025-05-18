260d425e42550f608464cb4780ac3ea06350f2cf
root <root@DESKTOP-SQPUCEH>
2025-05-19 00:20:32 +0300

    WORKING

--- MarketplaceInfrastructure/Program.cs
+++ MarketplaceInfrastructure/Program.cs
@@ -28,7 +28,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Queries}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
